76c44890d2956275144a5b639d4a57e3cfe31c88
muntehaahsan <munteha.ahsan@gmail.com>
2018-10-24 05:20:05 +0100

    Success and Failure paths implementaion with code matching

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,31 +3,32 @@ class UserController < ApplicationController
 
   #enter mobile number on index page
   def index
-    @exception_str = params[:exception_str]
+
   end
 
   #send user code to mobile number
   def user_code
-    client = ZenSend::Client.new("demo_kjMUHLoJTdz1paVgLLAKy0VA5jMJavNwKzlIj9NHq2M=")
-    begin
-      @random_str = [*('1'..'9')].sample(4).join
-      result = client.send_sms(
-          originator: "Zensend",
-          # Add your number here to send a message to yourself
-          # The number should be in international format.
-          # For example GB numbers will be 447400123456
-          numbers: [params[:phone]],
-          body: "Hello from Zensend! Enter #{@random_str} to verify your number"
-      )
-    rescue ZenSend::ZenSendException => e
-      puts "ZenSendException: #{e.parameter} => #{e.failcode}"
+    @random_str = [*('1'..'9')].sample(4).join
+    #client = ZenSend::Client.new("5pPy6oDeZgz9ZZ8YXxHfWQ")
+    #begin
+    #   result = client.send_sms(
+    #      originator: "SPINTOWIN",
+    #     # Add your number here to send a message to yourself
+    #   #The number should be in international format.
+    #   #For example GB numbers will be 447400123456
+    #   numbers: [params[:phone]],
+    #      body: "Hello from SPINTOWIN! Enter #{@random_str} to verify your number"
+    #)
+    #  flash[:notice] = 'A confirmation code has been sent on your mobile number sucessfully'
+    #rescue ZenSend::ZenSendException => e
+    #    redirect_to :root, alert: "ZenSendException: #{e.parameter} => #{e.failcode}"
+    #end
 
-    end
   end
 
   #verify user code sent on mobile number
   def verify_user
-
+       redirect_to :root, alert: "Oops! You have entered invalid code"
   end
 
 end
