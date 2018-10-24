4bfdd983c485ea1f374d0dcf83f8a4a5b4e8208a
muntehaahsan <munteha.ahsan@gmail.com>
2018-10-24 13:09:42 +0100

    integrated JQery plugin for international mobile number input

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,20 +9,7 @@ class UserController < ApplicationController
   #send user code to mobile number
   def user_code
     @random_str = [*('1'..'9')].sample(4).join
-    #client = ZenSend::Client.new("5pPy6oDeZgz9ZZ8YXxHfWQ")
-    #begin
-    #   result = client.send_sms(
-    #      originator: "SPINTOWIN",
-    #     # Add your number here to send a message to yourself
-    #   #The number should be in international format.
-    #   #For example GB numbers will be 447400123456
-    #   numbers: [params[:phone]],
-    #      body: "Hello from SPINTOWIN! Enter #{@random_str} to verify your number"
-    #)
-    #  flash[:notice] = 'A confirmation code has been sent on your mobile number sucessfully'
-    #rescue ZenSend::ZenSendException => e
-    #    redirect_to :root, alert: "ZenSendException: #{e.parameter} => #{e.failcode}"
-    #end
+    
 
   end
 
